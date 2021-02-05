3d216fe2fb9817d3039398e62178336b389f1685
Wiltrouble <wiltrouble@gmail.com>
2021-02-05 07:50:45 -0400

    Boards page update

--- src/main/java/org/example/pages/BoardsPage.java
+++ src/main/java/org/example/pages/BoardsPage.java
@@ -4,23 +4,31 @@ import org.example.pages.BoardPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import sun.awt.windows.WEmbeddedFrame;
 
 public class BoardsPage {
+
+    @FindBy(css = ".mod-add")
+    private WebElement createNewBoardBtn;
+
+    @FindBy(css = "input[data-test-id='create-board-title-input']")
+    private WebElement addBoardTitleTxt;
+
+    @FindBy(css = "button[data-test-id='create-board-submit-button']")
+    private WebElement createBoardBtn;
 
     private WebDriver driver;
 
     public BoardsPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public BoardPage createNewBoard(String boardName) {
-        WebElement createNewBoardBtn = driver.findElement(By.cssSelector(".mod-add"));
         createNewBoardBtn.click();
-
-        WebElement addBoardTitleTxt = driver.findElement(By.cssSelector("input[data-test-id='create-board-title-input']"));
         addBoardTitleTxt.sendKeys(boardName);
-
-        WebElement createBoardBtn = driver.findElement(By.cssSelector("button[data-test-id='create-board-submit-button']"));
         createBoardBtn.click();
         return new BoardPage(driver);
     }
